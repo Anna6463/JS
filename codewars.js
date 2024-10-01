ab19f71e002c8ff435c28e3cd1d91f0888911a68
Anna6463 <tvoianna@gmail.com>
2024-10-01 14:46:21 +0300

    change

--- codewars.js
+++ codewars.js
@@ -21,7 +21,7 @@ console.log(paperwork(10, 1));
 console.log(paperwork(-5, 1));
 console.log(paperwork(5, 5));*/
 
-function litres(time) {
+/*function litres(time) {
   var litrez = Math.floor(time * 0.5);
   if (litrez === 1) {
     return "1 litre";
@@ -33,3 +33,43 @@ function litres(time) {
 
 console.log(litres(3));
 console.log(litres(6.7));
+
+function simpleMultiplication(number) {
+  if (number % 2 ===0){
+    return  number * 8;
+  }
+  else{
+    return number * 9;
+  }
+}
+console.log(simpleMultiplication(12));
+console.log(simpleMultiplication(11));
+
+function sum (numbers) {
+  "use strict";
+  
+  var result = numbers.reduce(function(summa, current) {
+    return summa + current;
+  }, 0);
+  return result;
+  
+};
+console.log(sum([1, 5.2, 4, 0, -1]));
+
+const stringToNumber = function(str){
+  let num = Number(str);
+  return num;
+}
+
+function DNAtoRNA(dna) {
+  let a = dna.map(e => e.replace(/T/g, 'U'));
+  return ;
+}
+
+
+console.log(DNAtoRNA['TTTT',
+'GCAT',
+'GATTCCACCGACTTCCCAAGTACCGGAAGCGCGACCAACTCGCACAGC']);*/
+
+
+
